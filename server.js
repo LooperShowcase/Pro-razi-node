341dff12dd78d6a045f669ca07f7263bfbb55e9b
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2021-12-04 13:53:33 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,14 +2,40 @@ const fs = require("fs");
 let data = fs.readFileSync("db.json");
 let users = JSON.parse(data);
 
-function creatUser(id, name, city){
-users[id] = {
+function createUser(id, name, city) {
+  users[id] = {
     name,
-    city
-}
+    city,
+  };
 }
 
-createUser(1, "razi libbes", "madrid")
-createUser(1, "rolf lol", "lmfao")
+createUser(0, "razi libbes", "madrid");
+createUser(1, "rolf lol", "lmao");
 
 console.log(users);
+function readUser(id) {
+  return users[id];
+}
+
+console.log(users);
+
+function updateUser(id, name, city) {
+  if (users[id] == undefined) {
+    console.log("b3333333333333333");
+  } else {
+    users[id] = {
+      name: name,
+      city: city,
+    };
+  }
+}
+
+function deleteUser(id) {
+  delete users[id];
+}
+
+updateUser(0, "haha", "hehe", "lolol");
+
+//⛔🤚 STOP!!
+let dataToString = JSON.stringify(users);
+fs.writeFileSync("db.json", dataToString);
